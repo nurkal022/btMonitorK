382d01a4e1de0a67e700390c548a1476002f57eb
lukalua03 <nurkal022@gmail.com>
2023-08-27 14:37:12 +0600

    2 part mac list

---
+++ app/src/main/java/com/example/btmonitork/ListItem.kt
@@ -0,0 +1,7 @@
+package com.example.btmonitork
+
+
+data class ListItem(
+    var name: String,
+    var mac: String
+)
--- app/src/main/java/com/example/btmonitork/MainActivity.kt
+++ app/src/main/java/com/example/btmonitork/MainActivity.kt
@@ -9,32 +9,43 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.btmonitork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var btAdapter: BluetoothAdapter? = null
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: RcAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init(){
         val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         btAdapter = btManager.adapter
+        adapter= RcAdapter()
+        binding.rcView.layoutManager=LinearLayoutManager(this)
+        binding.rcView.adapter=adapter
         getPairedDevises()
     }
 
-    private fun   getPairedDevises(){
+    private fun  getPairedDevises(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(arrayOf(android.Manifest.permission.BLUETOOTH_CONNECT),1)
             }
         }
         val pairedDevices:Set<BluetoothDevice>?=btAdapter?.bondedDevices
+        val tempList=ArrayList<ListItem>()
         pairedDevices?.forEach{
-            Log.d("MyLog","Name:${it.name}")
+            tempList.add(ListItem(it.name,it.address))
+
+//            Log.d("MyLog","Name:${it.name}")
         }
+        adapter.submitList(tempList)
     }
 
 }
